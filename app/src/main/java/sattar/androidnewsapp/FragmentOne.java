d6ea63498ab3dfad646844a575b3f7e95c7ff4c9
Sattar <salambaev.sattar@gmail.com>
2017-10-02 00:28:22 +0600

    Circle image view and cards

--- app/src/main/java/sattar/androidnewsapp/FragmentOne.java
+++ app/src/main/java/sattar/androidnewsapp/FragmentOne.java
@@ -2,11 +2,19 @@ package sattar.androidnewsapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class FragmentOne extends Fragment {
 
@@ -23,13 +31,61 @@ public class FragmentOne extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View view =  inflater.inflate(R.layout.fragment_one, container, false);
+        View view =  inflater.inflate(R.layout.recycler_view, container, false);
+
+        List<String> list = new ArrayList<>();
+        list.add("Hello");
 
         RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
-
+        recyclerView.setAdapter(new RecyclerViewAdapter(list));
         return view;
     }
 
-}
+    private class RecyclerViewHolder extends RecyclerView.ViewHolder{
+        private CardView mCardView;
+        private TextView mTextView;
+        private CircleImageView mImgView;
 
+        public RecyclerViewHolder(View itemView){
+            super(itemView);
+        }
+
+        public RecyclerViewHolder(LayoutInflater inflater, ViewGroup container){
+            super(inflater.inflate(R.layout.card_view, container, false));
+
+            mCardView = itemView.findViewById(R.id.card_container);
+            mTextView = itemView.findViewById(R.id.text_holder);
+            mImgView = itemView.findViewById(R.id.img_holder);
+        }
+    }
+
+    private class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolder>{
+
+        List<String> mList;
+
+        public RecyclerViewAdapter(List<String> list){
+            this.mList = list;
+
+        }
+
+        @Override
+        public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+
+            return new RecyclerViewHolder(inflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+            holder.mImgView.setImageResource(R.drawable.drawner);
+            holder.mTextView.setText(mList.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mList.size();
+        }
+    }
+
+}
