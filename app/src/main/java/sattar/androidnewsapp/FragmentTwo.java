6ddeff3121b6d27e0380aa963a37ae9804cdb693
Sattar <salambaev.sattar@gmail.com>
2017-10-02 03:11:23 +0600

    Categories part added

--- app/src/main/java/sattar/androidnewsapp/FragmentTwo.java
+++ app/src/main/java/sattar/androidnewsapp/FragmentTwo.java
@@ -2,9 +2,20 @@ package sattar.androidnewsapp;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class FragmentTwo extends Fragment {
 
@@ -21,8 +32,66 @@ public class FragmentTwo extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_two, container, false);
+        View view =  inflater.inflate(R.layout.categories_recycler_view, container, false);
+
+        List<String> list = new ArrayList<>();
+        list.add("First");
+        list.add("SEcond");
+        list.add("Third");
+        list.add("Fourth");
+
+
+        int numberOfColumns = 2;
+        RecyclerView recyclerView = view.findViewById(R.id.categories_recycler_view);
+        recyclerView.setLayoutManager(new GridLayoutManager(getActivity(), numberOfColumns));
+        recyclerView.setAdapter(new RecyclerViewAdapter(list));
+        return view;
+    }
+
+    private class RecyclerViewHolder extends RecyclerView.ViewHolder{
+        private CardView mCardView;
+        private TextView mTextView;
+        private CircleImageView mImgView;
+
+        public RecyclerViewHolder(View itemView){
+            super(itemView);
+        }
+
+        public RecyclerViewHolder(LayoutInflater inflater, ViewGroup container){
+            super(inflater.inflate(R.layout.categories_card_view, container, false));
+
+            mCardView = itemView.findViewById(R.id.categories_card_container);
+            mTextView = itemView.findViewById(R.id.text_holder);
+            mImgView = itemView.findViewById(R.id.img_holder);
+        }
+    }
+
+    private class RecyclerViewAdapter extends RecyclerView.Adapter<RecyclerViewHolder>{
+
+        List<String> mList;
+
+        public RecyclerViewAdapter(List<String> list){
+            this.mList = list;
+
+        }
+
+        @Override
+        public RecyclerViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            LayoutInflater inflater = LayoutInflater.from(getActivity());
+
+            return new RecyclerViewHolder(inflater, parent);
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerViewHolder holder, int position) {
+            holder.mImgView.setImageResource(R.drawable.drawner);
+            holder.mTextView.setText(mList.get(position));
+        }
+
+        @Override
+        public int getItemCount() {
+            return mList.size();
+        }
     }
 
 }
-
